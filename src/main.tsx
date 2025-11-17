0bd73fd37589d93dbf14cb927a5654764b41c75c
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2025-11-17 22:09:16 +0800

    fixing some issue

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
+// @ts-ignore
 import './styles/globals.css'
 
 
