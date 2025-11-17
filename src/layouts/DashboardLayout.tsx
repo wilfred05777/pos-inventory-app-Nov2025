4334f342c5dc24575940c18427a991c28a71cdbd
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2025-11-17 22:25:40 +0800

    fixing example issue

---
+++ src/layouts/DashboardLayout.tsx
@@ -0,0 +1,13 @@
+import { Sidebar } from "../components/layout/Sidebar";
+
+export const DashboardLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <div className="flex h-screen">
+      <Sidebar />
+      <main className="flex-1 p-6 bg-gray-100 overflow-auto">
+        {children}
+      </main>
+    </div>
+  );
+};
+export default DashboardLayout;
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import POS from '../pages/pos/POS'
-import InventoryList from '../pages/inventory/InventoryList'
+import { Routes, Route } from "react-router-dom";
+import { DashboardLayout } from "../layouts/DashboardLayout";
+import POS from "../pages/pos/POS";
+import InventoryList from "../pages/inventory/InventoryList";
+
+export const AppRouter = () => (
+  <Routes>
+    <Route path="/pos" element={<DashboardLayout><POS /></DashboardLayout>} />
+    <Route path="/inventory" element={<DashboardLayout><InventoryList /></DashboardLayout>} />
+    {/* /products route removed because src/pages/products/ProductList.tsx is not a module */}
+    {/* Add more routes as needed */}
+  </Routes>
+);
+export default AppRouter;
+
+// import React from 'react'
+// import { Routes, Route, Navigate } from 'react-router-dom'
+// import POS from '../pages/pos/POS'
+// import InventoryList from '../pages/inventory/InventoryList'
 
 
-export default function AppRouter() {
-return (
-<Routes>
-<Route path="/" element={<Navigate to="/pos" replace />} />
-<Route path="/pos" element={<POS />} />
-<Route path="/inventory" element={<InventoryList />} />
-</Routes>
-)
-}
+// export default function AppRouter() {
+// return (
+// <Routes>
+// <Route path="/" element={<Navigate to="/pos" replace />} />
+// <Route path="/pos" element={<POS />} />
+// <Route path="/inventory" element={<InventoryList />} />
+// </Routes>
+// )
+// }
