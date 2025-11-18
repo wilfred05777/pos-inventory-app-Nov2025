a17a834d7465273681df7bb5564eaff269aae0a1
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2025-11-18 16:48:34 +0800

    fix the double display

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -29,42 +29,42 @@
 
 // export default AppRouter;
 
-import { Routes, Route, Navigate } from "react-router-dom";
-import { DashboardLayout } from "../layouts/DashboardLayout";
-import POS from "../pages/pos/POS";
-import InventoryList from "../pages/inventory/InventoryList";
+// import { Routes, Route, Navigate } from "react-router-dom";
+// import { DashboardLayout } from "../layouts/DashboardLayout";
+// import POS from "../pages/pos/POS";
+// import InventoryList from "../pages/inventory/InventoryList";
 
-export const AppRouter = () => (
-  <Routes>
-    <Route path="/" element={<Navigate to="/pos" replace />} />
-    <Route path="/pos" element={
-      <DashboardLayout>
-        <POS />
-      </DashboardLayout>} />
-    <Route path="/inventory" element={
-      <DashboardLayout>
-        <InventoryList />
-        </DashboardLayout>
-        } 
-        />
-    {/* /products route removed because src/pages/products/ProductList.tsx is not a module */}
-    {/* Add more routes as needed */}
-  </Routes>
-);
-export default AppRouter;
+// export const AppRouter = () => (
+//   <Routes>
+//     <Route path="/" element={<Navigate to="/pos" replace />} />
+//     <Route path="/pos" element={
+//       <DashboardLayout>
+//         <POS />
+//       </DashboardLayout>} />
+//     <Route path="/inventory" element={
+//       <DashboardLayout>
+//         <InventoryList />
+//         </DashboardLayout>
+//         } 
+//         />
+//     {/* /products route removed because src/pages/products/ProductList.tsx is not a module */}
+//     {/* Add more routes as needed */}
+//   </Routes>
+// );
+// export default AppRouter;
 
-// import React from 'react'
-// import { Routes, Route, Navigate } from 'react-router-dom'
-// import POS from '../pages/pos/POS'
-// import InventoryList from '../pages/inventory/InventoryList'
+import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import POS from '../pages/pos/POS'
+import InventoryList from '../pages/inventory/InventoryList'
 
 
-// export default function AppRouter() {
-// return (
-// <Routes>
-// <Route path="/" element={<Navigate to="/pos" replace />} />
-// <Route path="/pos" element={<POS />} />
-// <Route path="/inventory" element={<InventoryList />} />
-// </Routes>
-// )
-// }
+export default function AppRouter() {
+return (
+<Routes>
+<Route path="/" element={<Navigate to="/pos" replace />} />
+<Route path="/pos" element={<POS />} />
+<Route path="/inventory" element={<InventoryList />} />
+</Routes>
+)
+}
